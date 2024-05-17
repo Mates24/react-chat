b4ecfa54265b85bd839f04b08a94a4df08741f7d
Mates24 <matejcikmathias@gmail.com>
2024-05-17 14:38:15 +0200

    Styled body of app and imported components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import "./components/detail/Detail";
-import "./components/form/Form";
-import "./components/list/List";
+import Detail from "./components/detail/Detail";
+import Form from "./components/form/Form";
+import List from "./components/list/List";
+import "./index.css";
 
 const App = () => {
   return (
     <div className='container'>
-      
+      <Detail/>
+      <Form/>
+      <List/>
     </div>
   )
 }

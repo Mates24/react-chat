332f5ca7f102080096421781d40fcd6aa6d8201b
Mates24 <matejcikmathias@gmail.com>
2024-05-23 12:55:50 +0200

    Imported sections and styled it

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -1,8 +1,13 @@
 import "./list.css";
+import UserInfo from "./userInfo/userInfo";
+import ChatList from "./chatList/chatList";
 
 const List = () => {
     return(
-        <div className="list">List</div>
+        <div className="list">
+            <UserInfo/>
+            <ChatList/>
+        </div>
     )
 }
 

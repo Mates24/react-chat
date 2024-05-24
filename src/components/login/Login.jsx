c061c05ac4a82aadac828d2a1994b0a60aba0184
Mates24 <matejcikmathias@gmail.com>
2024-05-24 15:32:48 +0200

    Scrollbar styling

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -1,23 +1,37 @@
 import { useState } from "react";
-import "./login.css";
 import { toast } from "react-toastify";
+import "./login.css";
 
 const Login = () => {
     const [avatar, setAvatar] = useState({
         file: null,
         url: ""
-    })
+    });
     const handleAvatar = e => {
         if(e.target.files[0]){
             setAvatar({
                 file: e.target.files[0],
                 url: URL.createObjectURL(e.target.files[0])
         })}
-    }
+    };
 
     const handleLogin = e => {
         e.preventDefault();
-    }
+    };
+
+    const handleRegister = async (e) => {
+        e.preventDefault();
+        const formData = new FormData(e.target);
+
+        const { username, email, password } = Object.fromEntries(formData);
+
+        try{
+            
+        }catch(err){
+            console.log(err);
+            toast.error(err.message);
+        }
+    };
 
     return(
         <div className="login">
@@ -32,13 +46,13 @@ const Login = () => {
             <div className="separator"></div>
             <div className="item">
             <h2>Vytvoriť účet</h2>
-                <form>
+                <form onSubmit={handleRegister}>
                     <label htmlFor="file"><img src={avatar.url || "../../images/avatar.png"} alt="" />Nahrať fotografiu</label>
                     <input type="file" id="file" style={{display: "none"}} onChange={handleAvatar}/>
                     <input type="text" placeholder="Meno" name="username"/>
                     <input type="email" placeholder="Email" name="email"/>
                     <input type="password" placeholder="Heslo" name="password"/>
-                    <button>Registrovať sa</button>
+                    <button type="submit">Registrovať sa</button>
                 </form>
             </div>
         </div>

7cde911de63d6592fa701456811a28c5bf1ae286
Mates24 <matejcikmathias@gmail.com>
2024-06-03 16:30:53 +0200

    Created variable for userName fetch

--- src/components/list/userInfo/userInfo.jsx
+++ src/components/list/userInfo/userInfo.jsx
@@ -6,12 +6,13 @@ const userInfo = () => {
     const record = pocketbase.authStore.model;
     const avatarFileName = record?.avatar;
     const url = avatarFileName ? pocketbase.files.getUrl(record, avatarFileName, {'thumb': '100x250'}) : null;
+    const userName = pocketbase.authStore.model.name;
 
     return(
         <div className="userInfo">
             <div className="user">
                 <img src={url || ".././avatar.png"} alt=""/>
-                <h2>{pocketbase.authStore.model.name}</h2>
+                <h2>{userName}</h2>
             </div>
             <div className="icons">
                 <img src="./more.png" alt=""/>

e9b27580c699fc8bd9f025b6a4ca8713457e57a4
Mates24 <matejcikmathias@gmail.com>
2024-06-09 18:59:52 +0200

    Adding new chats

--- src/components/list/chatList/addUser/addUser.jsx
+++ src/components/list/chatList/addUser/addUser.jsx
@@ -7,6 +7,10 @@ const addUser = () => {
     const [url, setUrl] = useState(null);
     const [userName, setUserName] = useState(null);
 
+    const currentUserId = pocketbase.authStore.model.id;
+
+    const [receiverId, setReceiverId] = useState();
+
     const handleSearch = async (e) => {
         e.preventDefault();
 
@@ -21,19 +25,56 @@ const addUser = () => {
             const avatarFileName = record.avatar;
             const url = avatarFileName ? pocketbase.files.getUrl(record, avatarFileName, {'thumb': '100x250'}) : null;
             const userName = record.username;
-
-            setUrl(url);
-            setUserName(userName);
-
+            
             if(!searchedUser.empty){
+                setReceiverId(record.id);
+                
+                setUrl(url);
+                setUserName(userName);
                 setUser(searchedUser);
             }
+        }catch(err){
+            console.log(err);
+        };
+    };
 
+    const handleAdd = async () => {
+        try{
+            const date = new Date();
+            const chatsData = {
+                "messages": [],
+                "createdAt": date,
+            };
+            const chat = await pocketbase.collection('chats').create(chatsData);
+            
+            const userChatsData = {
+                "senderId": currentUserId,
+                "receiverId": receiverId,
+                "chats": chat.id,
+            };
+            const newUserChat = await pocketbase.collection('userChats').create(userChatsData);
+            
+            const currentUser = await pocketbase.collection('users').getOne(currentUserId);
+            const currentUserChats = currentUser.userchats;
+            console.log(currentUserChats);
+            const currentUserData = {
+                "userchats": [...currentUserChats, newUserChat.id]
+            };
+            await pocketbase.collection('users').update(currentUserId, currentUserData);
+            
+            const receiverUser = await pocketbase.collection('users').getOne(receiverId);
+            const receiverUserChats = receiverUser.userchats;
+            console.log(receiverUserChats);
+            const receiverUserData = {
+                "userchats": [...receiverUserChats, newUserChat.id]
+            };
+            await pocketbase.collection('users').update(receiverId, receiverUserData);
+            
         }catch(err){
             console.log(err);
         }
-    }
-
+    };
+    
     return(
         <div className="addUser">
             <form onSubmit={handleSearch}>
@@ -45,7 +86,7 @@ const addUser = () => {
                     <img src={url || "./avatar.png"} alt=""/>
                     <span>{userName}</span>
                 </div>
-                <button>Pridať</button>
+                <button onClick={handleAdd}>Pridať</button>
             </div>}
         </div>
     )
